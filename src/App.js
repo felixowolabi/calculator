5197d104f99316867215938213c28d7dc4bb836c
felixowolabi <flo.owol@gmail.com>
2022-12-30 19:00:02 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./styles.css";
 
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123,234 *</div>
+        <div className="current-operand">123,234</div>
       </div>
-      <div className="span-two">AC</div>
+      <button className="span-two">AC</button>
       <button>DEL</button>
       <button>÷</button>
       <button>1</button>
       <button>2</button>
+      <button>3</button>
       <button>*</button>
       <button>4</button>
       <button>5</button>
@@ -24,7 +25,7 @@ function App() {
       <button>-</button>
       <button>.</button>
       <button>0</button>
-      <button className="span-two">AC</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
