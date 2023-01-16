963ef2a472745d9a5ebf64820894c93f991dd016
felixowolabi <flo.owol@gmail.com>
2023-01-16 23:49:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="current-operand">123,234</div>
       </div>
       <button className="span-two">AC</button>
-      <button>Del </button>
+      <button>Delete </button>
       <button>÷</button>
       <button>1</button>
       <button>2</button>
