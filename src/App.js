2ee9db43cdd58debcdd5ffb00994d01cb5de7b6b
felixowolabi <flo.owol@gmail.com>
2023-01-17 22:14:46 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
       <button>3</button>
       <button>*</button>
       <button>4</button>
-      <button>4</button>
       <button>5</button>
       <button>6</button>
       <button>+</button>
